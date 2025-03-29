1234827cbfcd1eaa87370380c28a257c489bc3d9
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-03-29 15:50:50 +0700

    feat: add user dashboard

---
+++ PGTS_WPF/User/UserMainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PGTS_WPF.User
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class UserMainWindow : Window
+    {
+        public UserMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProfile_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+
+        private void btnSecurity_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+
+        private void btnPregnancies_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+
+        private void btnMilestones_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+
+        private void btnGrowthChart_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+    }
+}
